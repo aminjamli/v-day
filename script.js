238e55e5293cf5ca0db00582ecc32fdb0b4c40dd
amin jamli <aminjamli181@gmail.com>
2026-02-12 19:23:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const gifStages = [
     "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://tenor.com/lnNHiVniCFT.gif",  // 1 confused
+    "https://media.tenor.com/gq-TIxME3U8AAAAj/mitao-cat.gif",  // 1 confused
     "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
     "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
     "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif",  // 7 crying runaway
-    "https://tenor.com/dzSTRchev04.gif"
+    "https://media.tenor.com/J8ZohzxsHnQAAAAi/cat-tongue-shake-cat-tongue.gif"
+
 
 ]
 
